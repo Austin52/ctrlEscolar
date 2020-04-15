2c93c52ed09084d3aed8e0b31c37b0763e70a252
Austin <austintv52@gmail.com>
2020-04-14 21:37:03 -0500

    "Creacion de repositorio"

---
+++ index.php
@@ -0,0 +1,17 @@
+<!--
+	Vista Principal del Control Escolar 
+	Menu: home - docentes - alumnos - docentes
+	Select: contenido descritivo del funcionamiento del software
+	Pie: dirección - ubicacion - derechos
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Control Escolar</title>
+</head>
+<body>
+	<h1>View Index</h1>
+</body>
+</html>
